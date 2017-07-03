32c81ed336b4f5eb6cab8f65a5a1ef1bd071735d
kkpoon <noopkk@gmail.com>
2017-07-03 22:21:46 +0800

    Simple implementation

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # lambda-cors
+
+In AWS API Gateway, it could ONLY set one origin for CORS.
+
+To allow multiple origins, we could delay the CORS check to lambda execution.
+
+This library adds the CORS headers automatically to the response.
+
+```javascript
+var CORS = require("lambda-cors");
+
+var cors = CORS({
+    origins: ["http://domain1.com", "http://domain2.com"];
+})
+
+exports.handler = cors(function(event, context, callback) {
+    // ... your request handler
+
+    var res = { statusCode: 200, body: "Hello" };
+    callback(null, res);
+});
+```
+
+
---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import {
+    Handler,
+    APIGatewayEvent,
+    Context,
+    ProxyCallback
+} from "aws-lambda";
+
+export interface CorsOptions {
+    origins: string[];
+    allowCredentials: boolean;
+    allowMethod?: string[];
+    maxAge?: number;
+}
+
+export default (options: CorsOptions) => (handler: Handler) =>
+    (event: APIGatewayEvent, context: Context, callback: ProxyCallback) =>
+        handler(event, context, (err, res) => {
+            let matchedCORS = options.origins
+                .map(o => o.trim())
+                .filter(o => o === event.headers.origin);
+
+            if (matchedCORS.length > 0) {
+                res.headers = res.headers || {};
+                if (!!options.maxAge) {
+                    res.headers["Access-Control-Max-Age"] = options.maxAge;
+                }
+                res.headers['Access-Control-Allow-Headers'] =
+                    options.allowMethod ?
+                        options.allowMethod.join(",")
+                        : "GET,HEAD,PUT,PATCH,POST,DELETE";
+                res.headers['Access-Control-Allow-Credentials'] =
+                    JSON.stringify(!!options.allowCredentials);
+                res.headers['Access-Control-Allow-Origin'] =
+                    event.headers.origin;
+            }
+            callback(err, res);
+        });
